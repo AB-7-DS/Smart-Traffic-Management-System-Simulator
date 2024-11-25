bd1b31130b03b0abfca9a76ad89f013cfb42f8c4
Fatima Tu Zahra <156695063+hrafati-matuza@users.noreply.github.com>
2024-11-25 20:20:20 +0500

    added attributes to vehicle class

--- vehicle.h
+++ vehicle.h
@@ -9,26 +9,12 @@ using namespace std;
  * @brief A structure representing a vehicle with details about its route and priority.
  */
 struct Vehicle {
-    /**
-     * @brief The unique identifier for the vehicle.
-     */
-    string vehicleID;
-
-    /**
-     * @brief The starting intersection for the vehicle's route.
-     */
-    string startIntersection;
-
-    /**
-     * @brief The ending intersection for the vehicle's route.
-     */
-    string endIntersection;
-
-    /**
-     * @brief The priority level of the vehicle (e.g., high, low).
-     */
-    string priorityLevel;
-
+    string vehicleID; //<The unique identifier for the vehicle
+    const string startIntersection; //<The starting intersection for the vehicle's route never to be changed
+    const string endIntersection; //<The ending intersection for the vehicle's route never to be changed
+    string priorityLevel; //<The priority level of the vehicle (e.g., high, low)
+    string currentIntersectionId; //<The current intersection the vehicle is at. This will be updated as the vehicle moves.
+    string nextIntersectionId; //<The next intersection the vehicle will move to. This will be updated as the vehicle moves.
     /**
      * @brief Pointer to the next vehicle in a linked list.
      */
