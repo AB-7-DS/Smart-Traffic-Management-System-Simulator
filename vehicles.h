a6e608e876ac8af5a13b676f3dc52243be9f0639
Aleena Zahra <i232514@isb.nu.edu.pk>
2024-12-08 20:13:42 +0500

    added deoxygen comments

--- vehicles.h
+++ vehicles.h
@@ -6,35 +6,128 @@
 #include "vehicle.h"
 #include "Route.h"
 
+
 // All functions in this class should use std::string as parameter types.
+/**
+ * @class Vehicles
+ * @brief A class to manage a linked list of vehicles.
+ */
 class Vehicles {
 private:
     Vehicle* head; // Pointer to the head of the linked list
 
 public:
-    Vehicles(); // Constructor for Vehicles class.
-    ~Vehicles(); // Destructor for Vehicles class.
+/**
+ * @brief Constructor for Vehicles class.
+ */
+Vehicles();
 
-    // Function declarations that use std::string
-    void insertAtHead(std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
-    void enqueue(std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
-    bool insertAfterPosition(int position, std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
-    bool insertAfterID(std::string ID, std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
-    void deleteAtStart();
-    bool deleteAtEnd();
-    bool deleteAtIndex(int position);
-    bool deleteAtID(std::string ID);
-    bool isEmpty();
-    void printVehicles();
-    int findIDInVehicles(std::string vehicleID);
-    void loadAndReadCSVs();
-    Vehicle*& getHead();
-    // bool rerouteVehicleByID(const std::string& vehicleID, RerouteManager& rerouteManager);
-    // void recalculatePaths(RerouteManager& rerouteManager);
-    // bool updatePathForVehicle(const std::string& vehicleID, const std::string& newPath);
+/**
+ * @brief Destructor for Vehicles class.
+ */
+~Vehicles();
 
-    void addPaths(GPS& gps);
+/**
+ * @brief Inserts a vehicle at the head of the linked list.
+ * @param VehicleID The ID of the vehicle.
+ * @param startIntersection The starting intersection of the vehicle.
+ * @param endIntersection The ending intersection of the vehicle.
+ * @param priorityLevel The priority level of the vehicle.
+ */
+void insertAtHead(std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
 
+/**
+ * @brief Enqueues a vehicle at the end of the linked list.
+ * @param VehicleID The ID of the vehicle.
+ * @param startIntersection The starting intersection of the vehicle.
+ * @param endIntersection The ending intersection of the vehicle.
+ * @param priorityLevel The priority level of the vehicle.
+ */
+void enqueue(std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
+
+/**
+ * @brief Inserts a vehicle after a specific position in the linked list.
+ * @param position The position after which the vehicle will be inserted.
+ * @param VehicleID The ID of the vehicle.
+ * @param startIntersection The starting intersection of the vehicle.
+ * @param endIntersection The ending intersection of the vehicle.
+ * @param priorityLevel The priority level of the vehicle.
+ * @return True if the insertion was successful, false otherwise.
+ */
+bool insertAfterPosition(int position, std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
+
+/**
+ * @brief Inserts a vehicle after a specific vehicle ID in the linked list.
+ * @param ID The ID of the vehicle after which the new vehicle will be inserted.
+ * @param VehicleID The ID of the new vehicle.
+ * @param startIntersection The starting intersection of the new vehicle.
+ * @param endIntersection The ending intersection of the new vehicle.
+ * @param priorityLevel The priority level of the new vehicle.
+ * @return True if the insertion was successful, false otherwise.
+ */
+bool insertAfterID(std::string ID, std::string VehicleID, std::string startIntersection, std::string endIntersection, std::string priorityLevel);
+
+/**
+ * @brief Deletes the vehicle at the start of the linked list.
+ */
+void deleteAtStart();
+
+/**
+ * @brief Deletes the vehicle at the end of the linked list.
+ * @return True if the deletion was successful, false otherwise.
+ */
+bool deleteAtEnd();
+
+/**
+ * @brief Deletes the vehicle at a specific index in the linked list.
+ * @param position The index of the vehicle to be deleted.
+ * @return True if the deletion was successful, false otherwise.
+ */
+bool deleteAtIndex(int position);
+
+/**
+ * @brief Deletes the vehicle with a specific ID in the linked list.
+ * @param ID The ID of the vehicle to be deleted.
+ * @return True if the deletion was successful, false otherwise.
+ */
+bool deleteAtID(std::string ID);
+
+/**
+ * @brief Checks if the linked list is empty.
+ * @return True if the linked list is empty, false otherwise.
+ */
+bool isEmpty();
+
+/**
+ * @brief Prints the details of all vehicles in the linked list.
+ */
+void printVehicles();
+
+/**
+ * @brief Finds the position of a vehicle with a specific ID in the linked list.
+ * @param vehicleID The ID of the vehicle to be found.
+ * @return The position of the vehicle in the linked list, or -1 if not found.
+ */
+int findIDInVehicles(std::string vehicleID);
+
+/**
+ * @brief Loads and reads vehicle data from CSV files.
+ */
+void loadAndReadCSVs();
+
+/**
+ * @brief Gets the head of the linked list.
+ * @return A reference to the pointer to the head of the linked list.
+ */
+Vehicle*& getHead();
+
+/**
+ * @brief Adds paths to the vehicles using GPS data.
+ * @param gps The GPS object containing path data.
+ */
+void addPaths(GPS& gps);
+
+    
 };
 
 #endif
