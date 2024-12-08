25ea9fde3e6624b7459a8e264b7564dddcf281c2
Aleena Zahra <i232514@isb.nu.edu.pk>
2024-12-08 20:58:12 +0500

    undoing prior changes

--- visualizer.h
+++ visualizer.h
@@ -30,23 +30,26 @@ public:
  * This class is responsible for rendering the simulation of the traffic management system,
  * including roads and vehicles, using the SFML library.
  */
- 
+
 /**
  * @brief Constructor for the Visualizer class.
  *
  * Initializes the render window, loads textures, and sets up sprites.
  */
-    Visualizer();
+Visualizer();
 
 /**
  * @brief Draws the entire simulation graph.
  *
  * @param graph The graph representing the traffic network.
  * @param vehicles The collection of vehicles to be drawn.
+ * @param traffic The traffic light management system.
+ * @param ht The congestion monitoring system.
+ * @param accidentManager The accident management system.
  */
-    void drawSimulation(Graph &graph, Vehicles &vehicles,TrafficLightManagement &traffic,CongestionMonitoring &ht,Accident_roads &accidentManager);
+void drawSimulation(Graph &graph, Vehicles &vehicles, TrafficLightManagement &traffic, CongestionMonitoring &ht, Accident_roads &accidentManager);
 
-    /**
+/**
  * @brief Draws vehicles at the specified intersection.
  *
  * @param vehicles The collection of vehicles to be drawn.
@@ -54,12 +57,27 @@ public:
  * @param position The position where the vehicles should be drawn.
  * @param window The render window where the vehicles will be drawn.
  */
-   void drawVehicles(Vehicles &vehicles, const std::string &intersection,
-                            const sf::Vector2f &position, sf::RenderWindow &window);
+void drawVehicles(Vehicles &vehicles, const std::string &intersection, const sf::Vector2f &position, sf::RenderWindow &window);
 
+/**
+ * @brief Gets the elapsed time in seconds since the last clock reset.
+ *
+ * @return The elapsed time in seconds.
+ */
+float getElapsedTimeInSeconds();
 
-   float getElapsedTimeInSeconds();
-   sf::Color choseColor(EdgeNode *edge,Vertex *vertex,TrafficLightManagement &traffic,CongestionMonitoring &ht,Accident_roads &accidentManager);
- 
+/**
+ * @brief Chooses a color for the edge and vertex based on traffic conditions.
+ *
+ * @param edge The edge node representing the road segment.
+ * @param vertex The vertex representing the intersection.
+ * @param traffic The traffic light management system.
+ * @param ht The congestion monitoring system.
+ * @param accidentManager The accident management system.
+ * @return The chosen color.
+ */
+sf::Color choseColor(EdgeNode *edge, Vertex *vertex, TrafficLightManagement &traffic, CongestionMonitoring &ht, Accident_roads &accidentManager);
+
+   
 };
 #endif // VISUALIZER_H
