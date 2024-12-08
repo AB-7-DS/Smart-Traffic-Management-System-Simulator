81bbbcdc52997dbe28a926aecbdd2f58eb463273
Fatima Tu Zahra <156695063+hrafati-matuza@users.noreply.github.com>
2024-12-08 13:36:17 +0500

    updated documnentation in congestion max heap

--- congestionMaxHeap.h
+++ congestionMaxHeap.h
@@ -4,21 +4,64 @@
 # include "congestionMonitoring.h"
 
 class CongestionMaxHeap{
-      private:
-            RoadNode* root;
-      public:
-            CongestionMaxHeap();
-            void makeHeap(RoadNode* hashTableArray, int size);
-            void insert(RoadNode* newNode);
-            void heapifyUp(RoadNode*& newNode);
+    private:
+        RoadNode* root; //< Pointer to the root of the heap
 
-            void printHeap();
-            RoadNode* mostCongested();
-            void inorder(RoadNode* root);
-            void inorder();
-            void heapifyDown(RoadNode* node);
-            // void deleteMax();
-            // ~CongestionMaxHeap();
+        void inorder(RoadNode* root);
+    public:
+        /**
+         * @brief Construct a new Congestion Max Heap object
+         * 
+         * Initializes root to nullptr
+         * 
+         */
+        CongestionMaxHeap();
+        /**
+         * @brief Makes a max heap from the roads in the hashtable array
+         * 
+         * @param hashTableArray hashTableArray is from the congestion monitoring class
+         * @param size size of the hashtable array
+         */
+        void makeHeap(RoadNode* hashTableArray, int size);
+        /**
+         * @brief Inserts a new RoadNode into the heap
+         * 
+         * inserts preserving the structure property of the heap and heapifies up after insertion
+         * 
+         * @param newNode The RoadNode to be inserted
+         */
+        void insert(RoadNode* newNode);
+        /**
+         * @brief Heapifies up the heap after insertion
+         * 
+         * @param newNode The RoadNode to be heapified up
+         */
+        void heapifyUp(RoadNode*& newNode);
+        /**
+         * @brief Prints the heap 
+         * 
+         * Uses BFS to print the heap
+         * 
+         */
+        void printHeap();
+        /**
+         * @brief returns root of the heap without removing it
+         * 
+         * @return RoadNode* 
+         */
+        RoadNode* mostCongested();
+        /**
+         * @brief Prints the heap inorder
+         * 
+         * 
+         */
+        void inorder();
+        /**
+         * @brief Function to heapify down the heap
+         * 
+         * @param node 
+         */
+        void heapifyDown(RoadNode* node);
 
 };
 #endif
